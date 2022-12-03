1e26d255b631d6754c609d7ef9d00d8bf5254bb3
peteykienzzz <pmatczak2@gmail.com>
2022-12-03 11:03:11 -0800

    main project

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 building = {
     "building name": "andy",
+    "lobby and has no cubicles": 1,
     "floors": 10,
     "square foot per floor": 1000,
-    "cubicles": [6, 8],  # 6 feet by 8 feet
+    "restrooms per sq foot": 192,
+    "cubicles per sqf oot": 48,  # 6 feet by 8 feet
     "cubicles per floor": 20,
     "peak usage hours": [7, 8, 12, 13, 16, 17],
     "occupancy of each floor": 250,
