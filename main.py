5c2fe6419afa5a36b858c8f70221068c59ffc07b
peteykienzzz <pmatczak2@gmail.com>
2022-12-03 08:34:26 -0800

    data analysis for how many elevators should be in a building

---
+++ main.py
@@ -0,0 +1,9 @@
+building = {
+    "building name": "andy",
+    "floors": 10,
+    "square foot per floor": 1000,
+    "cubicles": [6, 8],  # 6 feet by 8 feet
+    "cubicles per floor": 20,
+    "peak usage hours": [7, 8, 12, 13, 16, 17],
+    "occupancy of each floor": 250,
+}
