9bd0a4fbefee6981072a3f5bab1d9d4a01ae48b7
peteykienzzz <pmatczak2@gmail.com>
2023-01-12 07:00:00 -0800

    main project elevator simulator

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-building = {
-    "building name": "andy",
-    "lobby and has no cubicles": 1,
-    "floors": 10,
-    "square foot per floor": 1000,
-    "restrooms per sq foot": 192,
-    "cubicles per sqf oot": 48,  # 6 feet by 8 feet
-    "cubicles per floor": 20,
-    "peak usage hours": [7, 8, 12, 13, 16, 17],
-    "occupancy of each floor": 250,
-}
+from loguru import logger
+
+
+logger.remove()
+logger.add("./sim_{time}.log", level="TRACE", rotation="100 MB")
+
+def clock_to_original_days(clock_time):
+    return (clock_time + 1) // 24
+
+def time_current_day(clock_time):
+    return (clock_time + 1) - (clock_to_original_days(clock_time) * 24)
+
+
+
+def run_sim(simulation):
+    logger.info(">>> RUN START\n")
+    for current_time in range(simulation['duration_hours']):
+        logger.info("Day: {}:{}", str(clock_to_original_days(current_time) + 100)[1:],
+                    str(time_current_day(current_time) + 100)[1:])
+
+
+def simulate():
+    simulation = {}
+    simulation['duration_days'] = 3
+    simulation['duration_hours'] = simulation['duration_days'] * 24
+    simulation['busy_periods'] = [7, 8, 11, 4, 5]
+    simulation["busy_weight"] = 1.5
+    simulation["number_of_floors"] = 10
+    simulation['occupancy_per_floor'] = 100
+    simulation['arrivals'] = simulation['number_of_floors']
+
+
+    run_sim(simulation)
+
+
+if __name__ == "__main__":
+    simulate()
